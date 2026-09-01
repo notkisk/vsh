cb9eb96017448ce10b48a402917c775d853844af
notkisk <salahxd99@gmail.com>
2026-09-02 00:41:59 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  std::cout << "$ ";
-  std::string userInput {};
-  std::getline(std::cin>>std::ws, userInput);
-  std::cerr << std::format("{}: command not found", userInput);
+  while(true){
+    std::cout << "$ ";
+    std::string userInput {};
+    std::getline(std::cin, userInput);
+    if(userInput.empty())
+      continue;
+    std::cerr << std::format("{}: command not found\n", userInput);
+    continue;
+  }
 }
