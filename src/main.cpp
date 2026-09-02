3b17a4556c903ac9c15e03d5a3b9c7a0f25898a9
notkisk <salahxd99@gmail.com>
2026-09-02 22:47:23 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,13 @@
 
 struct Path_Obj {
   const std::string_view ex_path;
-  const bool ex_permission;
 };
 
 bool checkExcuteAccessFromPath(const char* pathName) {
-  return access(pathName, X_OK);
+  return access(pathName, X_OK) == 0;
 }
 
-bool checkPath(std::string_view command) {
-  const char* path = std::getenv("PATH");
-  const std::string_view path_view{path};
-  bool does_exits{path_view.find(command) != std::string_view::npos};
-  std::cout << std::boolalpha;
-  DEBUG_PRINT(path);
-  DEBUG_PRINT(std::format("excutable exists {}", does_exits));
-  DEBUG_PRINT(path);
-  
-  return does_exits;
-}
-
-std::pair<std::vector<Path_Obj>, uint16_t> retrievePath() {
+std::vector<Path_Obj> retrievePath() {
   const char* path = std::getenv("PATH");
   const std::string_view path_view{path};
   std::size_t start{0};
@@ -46,51 +33,70 @@ std::pair<std::vector<Path_Obj>, uint16_t> retrievePath() {
   for (std::size_t i = 0; i <= path_view.size(); ++i) {
     if (i == path_view.size() || path_view[i] == ':') {
       std::string_view path_part = path_view.substr(start, i - start);
+
       paths.push_back(
         Path_Obj{
-          path_part,
-          checkExcuteAccessFromPath(std::string(path_part).c_str())
+          path_part
         }
       );
+
       start = i + 1;
     }
   }
 
-  return {paths, paths.size() == 0 ? 0 : 1};
+  return paths;
 }
-
 
 int main() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    auto [paths, path_status] = retrievePath();
+
+    auto paths = retrievePath();
+
     while (true) {
         std::cout << "$ ";
+
         std::string userInput{};
         std::getline(std::cin, userInput);
+
         if (userInput == "exit")
             break;
+
         if (userInput.empty())
             continue;
-        // This is just for now, later we need a more rigrouds parsing, a function that strips away spaces and new lines
-    // for better command interpertation
-        if (userInput == "echo" || userInput == "echo\n"  || userInput.starts_with("echo ")) {
-            std::cout << userInput.substr(4 + (userInput.size() > 4 && userInput[4] == ' ')) << '\n';
+
+        // This is just for now, later we need a more rigorous parsing,
+        // a function that strips away spaces and new lines
+        // for better command interpretation
+
+        if (userInput == "echo" ||
+            userInput.starts_with("echo ")) {
+            if (userInput == "echo") {
+                std::cout << '\n';
+            } else {
+                std::cout << userInput.substr(5) << '\n';
+            }
             continue;
         }
         if (userInput.starts_with("type ")) {
             std::string command = userInput.substr(5);
-            if (command == "echo" || command == "exit" || command == "type") {
+
+            if (command == "echo" ||
+                command == "exit" ||
+                command == "type") {
                 std::cout << command << " is a shell builtin\n";
             } else {
                 bool found{false};
                 for (const auto& path : paths) {
-                    if (!path.ex_permission)
-                        continue;
                     std::string full_path =
                         std::format("{}/{}", path.ex_path, command);
-                    if (checkExcuteAccessFromPath(full_path.c_str()) == 0) {
-                        std::cout << std::format("{} is {}\n", command, full_path);
+
+                    if (checkExcuteAccessFromPath(full_path.c_str())) {
+                        std::cout << std::format(
+                            "{} is {}\n",
+                            command,
+                            full_path
+                        );
                         found = true;
                         break;
                     }
@@ -98,6 +104,7 @@ int main() {
                 if (!found)
                     std::cout << command << ": not found\n";
             }
+
             continue;
         }
         std::cerr << std::format("{}: command not found\n", userInput);
