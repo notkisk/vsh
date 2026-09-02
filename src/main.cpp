ca4a4fa73d78aa4f6a7a7c6896b890e247ddf928
notkisk <salahxd99@gmail.com>
2026-09-02 01:12:34 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@ int main() {
       break;
     if(userInput.empty())
       continue;
+    if (userInput.starts_with("echo ")) {
+      std::cout << userInput.substr(5) << '\n';
+      continue;
+}
     std::cerr << std::format("{}: command not found\n", userInput);
   }
 }
