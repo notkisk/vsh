f4fc5cc2f8374349879215a0ba96d71cb3f29e3a
notkisk <salahxd99@gmail.com>
2026-09-02 01:27:32 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,30 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  while(true){
-    std::cout << "$ ";
-    std::string userInput {};
-    std::getline(std::cin, userInput);
-    if (userInput == "exit")
-      break;
-    if(userInput.empty())
-      continue;
-    if (userInput.starts_with("echo ")) {
-      std::cout << userInput.substr(5) << '\n';
-      continue;
-}
-    std::cerr << std::format("{}: command not found\n", userInput);
-  }
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    while (true) {
+        std::cout << "$ ";
+        std::string userInput{};
+        std::getline(std::cin, userInput);
+        if (userInput == "exit")
+            break;
+        if (userInput.empty())
+            continue;
+        if (userInput.starts_with("echo ")) {
+            std::cout << userInput.substr(5) << '\n';
+            continue;
+        }
+        if (userInput.starts_with("type ")) {
+            std::string command = userInput.substr(5);
+            if (command == "echo" || command == "exit" || command == "type") {
+                std::cout << command << " is a shell builtin\n";
+            } else {
+                std::cout << command << ": not found\n";
+            }
+            continue;
+        }
+        std::cerr << std::format("{}: command not found\n", userInput);
+    }
 }
